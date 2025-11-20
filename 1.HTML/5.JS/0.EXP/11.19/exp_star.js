b488abe164eb41e93f6fcc4ed28090253d60b109
FatBuu <jbgong029@gmail.com>
2025-11-20 18:12:12 +0900

    [기능] 과제 - 11.20 - 별 그리기(변수 입력) - 업 앤 다운 카운트 게임 - 계산기 프로그램

--- 1.HTML/5.JS/0.EXP/11.19/exp_star.js
+++ 1.HTML/5.JS/0.EXP/11.19/exp_star.js
@@ -1,10 +1,14 @@
 // 1번 예제
-for (i = 0; i < 5; i++) {
+for (i = 1; i <= 5; i++) {
     let line = '';
     for (j = 0; j <= i; j++) {
         line += '*';
     }
     console.log(line);
+}
+// 개선된 코드
+for (i = 1; i <= 5; i++) {
+    console.log("*".repeat(i));
 }
 
 // 2번 예제
@@ -14,6 +18,9 @@ for (i = 5; i > 0; i--) {
         line += '*';
     }
     console.log(line);
+}
+for (i = 5; i >= 1; i--) {
+    console.log("*".repeat(i));
 }
 
 // 3번 예제
@@ -27,6 +34,10 @@ for (i = 0; i < 5; i++) {
     }
     console.log(line);
 }
+console.log("3. 오른쪽 삼각형 그리기 - 개선된 코드");
+for (let i = 1; i <= 5; i++) {
+    console.log(" ".repeat(5-i) + "*".repeat(i));
+}
 
 // 4번 예제
 for (i = 5; i > 0; i--) {
@@ -38,4 +49,8 @@ for (i = 5; i > 0; i--) {
         line += '*';
     }
     console.log(line);
+}
+console.log("4. 오른쪽 역삼각형 그리기 - 개선된 코드");
+for (let i = 5; i >= 1; i--){
+    console.log(" ".repeat(5-i) + "*".repeat(i));
 }
